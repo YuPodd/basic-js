68171cc6e20260c29fc9706bf28c382c6bac2adf
Yuliya <yulia.podd@gmail.com>
2020-11-01 13:43:32 +0200

    feat:add cats, dream-team, hanoi, season functions

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if (members === null || typeof members !== 'object' || members.length === 0) {
+    return false;
+  }
+  let arr = [];
+  let result = '';
+  for(let i = 0; i < members.length; i++){
+    if (typeof members[i]==='string'){
+  arr.push(members[i].trim().toUpperCase().charAt(0));
+    }
+  }
+  return result = arr.sort().join('');
+  };
+  
+  
