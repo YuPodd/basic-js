ecbee98409bd67a7878e41210dc144d2b44f589c
Yuliya <yulia.podd@gmail.com>
2020-11-01 21:30:58 +0200

    feat:add carbon function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,5 +4,9 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
+    if (typeof sampleActivity !== 'string') return false;
+    sampleActivity = parseFloat(sampleActivity);
+    if (!sampleActivity || sampleActivity <= 0 || sampleActivity > MODERN_ACTIVITY) return false;
   
+    return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) * HALF_LIFE_PERIOD / 0.693);
 };
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  // calculateDepth(arr) {
+  //   if(!arr){
+  //     return false;
+  //   }
+  //    this.calculateDepthInner(arr);
+  // }
+
+  calculateDepth(arr) {
+    return this.calculateDepth(arr,1);
+  };
+    
+  calculateDepth(arr, d = 1) {
+    let isArray = Array.isArray(arr);
+    if (!isArray) {
+      return d;
+    }
+
+    let max = d+1;
+    for (let i = 0; i < arr.length; i++) {
+      let depthNested = this.calculateDepth(arr[i], d + 1);
+      if (depthNested > max)
+        max = depthNested;
+    }
+    return max;
   }
 };
